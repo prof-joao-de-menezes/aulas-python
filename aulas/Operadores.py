17c3a85b2c303d100f710f28f7edabfa0b2ee767
Joao <joao.m.junior7@docente.senai.br>
2026-09-08 18:43:16 -0300

    atualizando repositório

---
+++ aulas/Operadores.py
@@ -0,0 +1,61 @@
+#OPERADORES MATEMÁTICOS
+numero1 = 10
+numero2 = 20
+
+#SOMA
+soma = numero1 + numero2
+#SUBTRAÇÃO
+subtracao = numero1 - numero2
+#MULTIPLICAÇÃO
+multiplicacao = numero1 * numero2
+#DIVISÃO
+divisao = numero1 / numero2 #3 / 2 -> 1.5
+#DIVISÃO SEM NUMERO QUEBRADO
+divisao_sem_numero_quebrado = numero1 // numero2 #3 / 2 -> 1
+#RESTO
+resto = numero1 % numero2 # 11 % 2 -> 1
+
+print("Soma dos números: ", soma)
+print("Subtração dos números: ", subtracao)
+print("Multiplicação dos números: ", multiplicacao)
+print("Divisão dos números: ", divisao)
+print("Divisão dos números: ", divisao_sem_numero_quebrado)
+print("Resto dos números: ", resto)
+
+"""
+    # OPERADORES RELACIONAIS
+    esperar uma resposta de True ou False
+    != -> se for diferente retorna True, se for igual retorna False
+    == -> se for diferente retorna False, se for igual retorna True
+    
+    > -> se for maior retorna True, se for menor retorna False
+    < -> se for menor retorna False, se for igual retorna True
+    
+    >= -> se for maior E igual retorna True, contrario retorna False
+    <= -> se for menor E igual retorna True, contrario retorna False
+
+--------------------------------------------------------------------------
+
+    #OPERADORES LÓGICOS
+    and -> se todas as comparações forem True, retorna True
+    or -> se ao menos uma das comparações for Truwe, retorna True
+    not
+    
+--------------------------------------------------------------------------
+
+    # OPERADORES DE ATRIBUIÇÃO
+    = 
+    +=
+    -=
+    *=
+    /=
+    
+"""
+
+idade = 18
+
+# print(idade != 18) # False
+# print(idade == 18) # True
+# print(idade > 18)  # False
+# print(idade < 18)  # False
+# print(idade >= 18) # True
