9a65c51d384a1e6304318023e5234b2cdcba9ec1
Joao <joao.m.junior7@docente.senai.br>
2026-09-08 20:03:07 -0300

    atualizando repositório

--- aulas/Operadores.py
+++ aulas/Operadores.py
@@ -15,12 +15,12 @@ divisao_sem_numero_quebrado = numero1 // numero2 #3 / 2 -> 1
 #RESTO
 resto = numero1 % numero2 # 11 % 2 -> 1
 
-print("Soma dos números: ", soma)
-print("Subtração dos números: ", subtracao)
-print("Multiplicação dos números: ", multiplicacao)
-print("Divisão dos números: ", divisao)
-print("Divisão dos números: ", divisao_sem_numero_quebrado)
-print("Resto dos números: ", resto)
+# print("Soma dos números: ", soma)
+# print("Subtração dos números: ", subtracao)
+# print("Multiplicação dos números: ", multiplicacao)
+# print("Divisão dos números: ", divisao)
+# print("Divisão dos números: ", divisao_sem_numero_quebrado)
+# print("Resto dos números: ", resto)
 
 """
     # OPERADORES RELACIONAIS
@@ -44,15 +44,24 @@ print("Resto dos números: ", resto)
 --------------------------------------------------------------------------
 
     # OPERADORES DE ATRIBUIÇÃO
-    = 
-    +=
-    -=
-    *=
-    /=
+    = -> atribui um valor à variável
+    += -> atribuir um valor X NUMÉRICO à variável
+    -= -> subtrair um valor X NUMÉRICO da variável
+    *= -> mutiplicar um valor X pelo NÚMERO da variável
+    /= -> dividir um valor X pelo NÚMERO da variável
     
 """
 
-idade = 18
+idade = 10
+print("O valor da idade é: ",idade)
+idade += 8 # 18 -> incremento
+print("O valor da idade é: ",idade)
+idade -= 8 # 10 -> decremento
+print("O valor da idade é: ",idade)
+idade *= 2 # 20
+print("O valor da idade é: ",idade)
+idade /= 2 # 10
+print("O valor da idade é: ",idade)
 
 # print(idade != 18) # False
 # print(idade == 18) # True
