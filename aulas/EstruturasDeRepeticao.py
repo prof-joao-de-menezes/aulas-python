2d92a6480b25a9c44e12dc75e0d0e6980a588c28
Joao <joao.m.junior7@docente.senai.br>
2026-09-08 20:39:01 -0300

    atualizando repositório

--- aulas/EstruturasDeRepeticao.py
+++ aulas/EstruturasDeRepeticao.py
@@ -1,4 +1,7 @@
 # REPETIÇÃO WHILE -> Enquanto
+# Estruturas de repetição
+# Laços de repetição
+# Loop
 
 # ano_nascimento = 2004
 # ano_final = 2077
@@ -11,17 +14,38 @@
 #     print(idade)
 #     ano_nascimento += 1  # incremento
 #
-# print("O ano atual é: ", ano_nascimento)
+# print("O ano atual é: ", ano_nascimento
+
+# seu_nome = input("Digite seu nome: ")
+#
+# while seu_nome != "Joao":
+#     print("Pessoa não encontrada....")
+#     seu_nome = input("Digite seu nome novamente: ")
 
 
+fichas = 2
+while fichas != 0: # sistema da maquina do fliperama
+    tentativas = 3
+
+    print(f"Você tem {fichas} quantidade de fichas.")
+    while True: # sistema do jogo do fliperama
+        print(f"Você tem {tentativas} tentativas.")
+        opcao = input("Escolha uma opção:")
+        if opcao == "b":
+            print("Você ganhou!")
+            fichas = 0
+            break
+        elif tentativas == 1:
+            print("Você perdeu")
+            break
+        else:
+            tentativas -= 1
+
+    fichas -= 1
+    if fichas <= 0:
+        break
 
 
-
-seu_nome = input("Digite seu nome: ")
-
-while seu_nome != "Joao":
-    print("Pessoa não encontrada....")
-    seu_nome = input("Digite seu nome novamente: ")
 
 
 
