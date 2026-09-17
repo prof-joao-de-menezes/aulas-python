d74472f3c41c42581c0441d7a57ecd475ada379c
Joao <joao.m.junior7@docente.senai.br>
2026-09-17 19:40:28 -0300

    atualizando repositório

--- atividades_poo/atividade1/Carro.py
+++ atividades_poo/atividade1/Carro.py
@@ -3,7 +3,7 @@ class Carro:
         self.__dono = dono # Privado -> _Carro__dono
         self.cor = cor # Pública
         self.tipo = tipo
-        self.modelo = modelo
+        self.__modelo = modelo # _Carro__modelo
         self.ano = ano
         self.fabricante = fabricante
 
@@ -12,18 +12,21 @@ class Carro:
                 f"\n\tDono (valor classe): {self._Carro__dono}"
                 f"\n\tCor: {self.cor}"
                 f"\n\tTipo: {self.tipo}"
-                f"\n\tModelo: {self.modelo}"
+                f"\n\tModelo: {self.__modelo}"
                 f"\n\tAno: {self.ano}"
                 f"\n\tFabricante: {self.fabricante}")
 
     def acelerar(self): # retorna um valor
-        print(f"O {self.__dono} acelerou o {self.modelo}")
+        print(f"O {self.__dono} acelerou o {self.__modelo}")
 
     def frear(self): # metodos sem return, são vazios
         print("O carro freiou")
 
     def buzinar(self):
         print("O carro buzinou")
+
+    def bater(self):
+        self.__modelo = "Sucata."
 
 #lado de fora da Classe
 carro_gustavo = Carro(
@@ -44,10 +47,18 @@ carro_vinicius = Carro("Vinicius","Amarelo", "Popular", "Camaro", 2025, "GM")
 #                      int(input("Qual o ano do carro: ")),
 #                      input("Qual o fabricante do carro: "))
 
-carro_victor = Carro("Victor", "Azul", "Popular", "BYD", 2025, "GM")
-carro_victor.dono = "João"
-print(carro_victor.dono)
-carro_victor.__dono = "Felipe"
-print(carro_victor.__dono)
-
-print(carro_victor._Carro__dono)
+carro_victor = Carro("Victor",
+                     "Azul",
+                     "Popular",
+                     "BYD",
+                     2025,
+                     "GM")
+print(carro_victor)
+carro_victor.modelo = "Ferrari"
+carro_victor.__modelo = "Ferrari"
+carro_victor._Carro__modelo = "Ferrari"
+print(carro_victor)
+#imprime até o name mangling -> PRIVADO
+# __modelo
+# _Carro_modelo
+print(carro_victor.__dict__)
