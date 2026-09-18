c587ec1c446a3c72c08ca1f01041ec5980ffc5cb
Joao <joao.m.junior7@docente.senai.br>
2026-09-18 20:04:57 -0300

    atualizando repositório

--- atividades_poo/atividade2/Produto.py
+++ atividades_poo/atividade2/Produto.py
@@ -4,6 +4,30 @@ class Produto:
         self.__preco = preco
         self.__quantidade_estoque = quantidade_estoque
 
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome
+
+    @property
+    def preco(self):
+        return self.__preco
+
+    @preco.setter
+    def preco(self, novo_preco):
+        self.__preco = novo_preco
+
+    @property
+    def quantidade_estoque(self):
+        return self.__quantidade_estoque
+
+    @quantidade_estoque.setter
+    def quantidade_estoque(self, novo_quantidade_estoque):
+        self.__quantidade_estoque = novo_quantidade_estoque
+
     def adicinar_estoque(self, quantidade):
         if quantidade > 0:
             self.__quantidade_estoque += quantidade
@@ -18,4 +42,7 @@ shampoo = Produto("Shampoo",
 shampoo.__quantidade_estoque = -50
 shampoo.__preco = -100
 
+print(shampoo.__dict__)
+
+shampoo.nome = "Nivea"
 print(shampoo.__dict__)
